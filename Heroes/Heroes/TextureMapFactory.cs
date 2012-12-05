c82b07e5de75cf5097ef3af52e2736ca0423002e
pennetti <tjp7mb@virginia.edu>
2012-12-05 10:11:46 -0500

    Added TextureMapFactory [in progress]

---
+++ Heroes/Heroes/TextureMapFactory.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Heroes
+{
+    public class TextureMapFactory : Microsoft.Xna.Framework.GameComponent
+    {
+        public TextureMapFactory(Game game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
